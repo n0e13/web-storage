5a9714c127ba97e7e7914900a0e6ce674f8326b3
airin181 <98608110+airin181@users.noreply.github.com>
2022-02-28 13:59:26 +0100

    UPDATE: script order in html

--- js/script.js
+++ js/script.js
@@ -50,58 +50,6 @@ function writeUser() {
 
 /*________FIREBASE________*/
 
-document.querySelector("form[name='save_data']").addEventListener('submit', function (event) {
-
-  event.preventDefault();
-
-  const name = event.target.name.value;
-  const email = event.target.email.value;
-  const message = event.target.message.value;
-  const image = event.target.img.value;
-
-  saveUser(name, email, message, image);
-})
-
-function saveUser(name, email, message, image) {
-  // Primero leemos si hay algo almacenado y si no creamos un array vacío
-  let aUsers = JSON.parse(localStorage.getItem("user") || "[]");
-
-  // creo el nuevo objeto usuario
-    let oNewUser = {
-      name: name,
-      email: email,
-      message: message,
-      image: image
-    };
-
-    // añado el objeto al array
-    aUsers.push(oNewUser);
-
-    //Escribir
-    localStorage.setItem(
-      "user",
-      JSON.stringify(aUsers)
-    );
-
-    // Llamamos a la función escribir para que lo muestre en pantalla
-    writeUser();
-}
-
-function writeUser() {
-  let contactSection = document.getElementById("contacts-section");
-
-  //Leer
-  if(localStorage.length > 0) {
-    let user = JSON.parse(localStorage.getItem("user"));
-    contactSection.innerHTML = `<h1>Estos son los contactos guardados:</h1>`;
-    for(let i = 0; i < user.length; i++){
-      contactSection.innerHTML += `<p>Usuario: ${user[i].name}, E-mail: ${user[i].email}, Foto: ${user[i].image}, Mensaje: ${user[i].message}</p>`
-    }
-  }
-}
-
-
-
 // Your web app's Firebase configuration
 let firebaseConfig = {
   // Tu objeto de configuración aquí
@@ -112,56 +60,84 @@ let firebaseConfig = {
   storageBucket: "pruebaweb-cf4a9.appspot.com",
   messagingSenderId: "95460811666",
   appId: "1:95460811666:web:a13a5d9fa53b494612174b"
-  
- };
+
+};
 
 
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- const db = firebase.firestore();
- const createUser = (user) => {
-   db.collection("users")
-     .add(user)
-     .then((docRef) => console.log("Document written with ID: ", docRef.id))
-     .catch((error) => console.error("Error adding document: ", error));
- };
- document.getElementById("crear").addEventListener("click", () => {
-   const first = prompt("Introduce nombre");
-   const last = prompt("introduce apellido");
-   createUser({
-     first,
-     last,
-     born: 1191,
-     otro: "cafe",
-   });
- });
- const readAllUsers = (born) => {
-   db.collection("users")
-     .where("born", "==", born)
-     .get()
-     .then((querySnapshot) => {
-       querySnapshot.forEach((doc) => {
-         console.log(doc.data());
-       });
-     });
- };
- //readAllUsers(1224)
- // Read ONE
- //busca por id y cuando lo tengas dime lo que hay.
- function readOne(id) {
-   db.collection("users")
-     .doc(id)
-     .get()
-     .then((doc) => {
-       if (doc.exists) {
-         console.log("Document data:", doc.data());
-       } else {
-         // doc.data() will be undefined in this case
-         console.log("No such document!");
-       }
-     })
-     .catch((error) => {
-       console.log("Error getting document:", error);
-     });
- }
- //readOne("690WYQfTZUoEFnq5q1Ov");
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const createUser = (user) => {
+  db.collection("usuarios-firebase")
+    .add(user)
+    .then((docRef) => console.log("Document written with ID: ", docRef.id))
+    .catch((error) => console.error("Error adding document: ", error));
+};
+
+document.querySelector("form[name='save_data']").addEventListener('submit', function (event) {
+
+  event.preventDefault();
+
+  const name = event.target.name.value;
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+  const image = event.target.img.value;
+
+  createUser({
+    email: email,
+    foto: image,
+    mensaje: message,
+    nombre: name
+  });
+
+})
+
+
+
+
+
+
+
+
+
+
+/* document.getElementById("crear").addEventListener("click", () => {
+  const first = prompt("Introduce nombre");
+  const last = prompt("introduce apellido");
+  createUser({
+    first,
+    last,
+    born: 1191,
+    otro: "cafe",
+  });
+});
+const readAllUsers = (born) => {
+  db.collection("users")
+    .where("born", "==", born)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+};
+//readAllUsers(1224)
+// Read ONE
+//busca por id y cuando lo tengas dime lo que hay.
+function readOne(id) {
+  db.collection("users")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+}
+//readOne("690WYQfTZUoEFnq5q1Ov"); */
