62bfc0422df26debd465a02575008290dfe814db
airin181 <98608110+airin181@users.noreply.github.com>
2022-02-28 12:59:20 +0100

    ADD: preventD.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 document.querySelector("form[name='save_data']").addEventListener('submit', function (event) {
 
+  event.preventDefault();
+
   const name = event.target.name.value;
   const email = event.target.email.value;
   const message = event.target.message.value;
+  const image = event.target.img.value;
 
-  saveUser(name, email, message);
+  saveUser(name, email, message, image);
 
 })
 
-function saveUser(name, email, message) {
+function saveUser(name, email, message, image) {
   // Primero leemos si hay algo almacenado y si no creamos un array vacío
   let aUsers = JSON.parse(localStorage.getItem("user") || "[]");
 
   // creo el nuevo objeto usuario
     let oNewUser = {
-      "nameKey": name,
-      "emailKey": email,
-      "messageKey": message
+      name: name,
+      email: email,
+      message: message,
+      image: image
     };
 
     // añado el objeto al array
@@ -29,14 +33,20 @@ function saveUser(name, email, message) {
     );
 
     // Llamamos a la función escribir para que lo muestre en pantalla
-    writeUser()
+    writeUser();
 }
 
 function writeUser() {
-  let contactSection = document.getElementsByClassName("contacts-section");
+  let contactSection = document.getElementById("contacts-section");
 
   //Leer
-  let user = JSON.parse(localStorage.getItem("user") || "[]");
-  contactSection.innerHTML = `<h1>Estos son los contactos guardados:</h1><p>Usuario: ${user.nameKey}, E-mail: ${user.emailKey}, Mensaje: ${messageKey}</p>`
-
+  if(localStorage.length > 0) {
+    let user = JSON.parse(localStorage.getItem("user"));
+    contactSection.innerHTML = `<h1>Estos son los contactos guardados:</h1>`;
+    for(let i = 0; i < user.length; i++){
+      contactSection.innerHTML += `<p>Usuario: ${user[i].name}, E-mail: ${user[i].email}, Foto: ${user[i].image}, Mensaje: ${user[i].message}</p>`
+    }
+  }
 }
+
+
